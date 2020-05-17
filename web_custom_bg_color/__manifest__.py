ec67a3befd24f34b0ff4da99bb62b07117f15e97
clementmbr <clement.mombereau@akretion.com.br>
2020-05-17 01:27:46 -0300

    [ADD] web_custom_bg module

---
+++ web_custom_bg_color/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2017 - 2018 Modoolar <info@modoolar.com>
+# Copyright 2018 Brainbean Apps
+# License LGPLv3.0 or later (https://www.gnu.org/licenses/lgpl-3.0.en.html).
+
+{
+    "name": "Web Models custom background color",
+    "summary": "Enable to change some models form's background color",
+    "category": "Web",
+    "version": "12.0.0.1.0",
+    "license": "LGPL-3",
+    "author": "Clément Mombereau",
+    "website": "https://github.com/clementmbr/crm-booking",
+    "depends": [
+        "web",
+        "backend_theme_v12",  # https://github.com/Openworx/backend_theme
+    ],
+    "data": ["views/web_custom_bg_color.xml"],
+    "installable": True,
+}
