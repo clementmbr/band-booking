8827eaa494fef7fd2a899fc46e36b0edf7becef4
clementmbr <clement.mombereau@akretion.com.br>
2020-05-23 22:54:36 -0300

    [ADD] Demo Festivals with images

--- crm_booking/hooks.py
+++ crm_booking/hooks.py
@@ -1,6 +1,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+import base64
 import logging
+from os import listdir
 
 from lxml import etree
 
@@ -27,10 +29,11 @@ def pre_init_hook(cr):
 
 
 def post_init_hook(cr, registry):
-    """Delete Odoo unuseful native CRM records and display Leads"""
     env = api.Environment(cr, SUPERUSER_ID, {})
     _logger.info(_("Preparing datas for 'crm_booking'..."))
 
+    # Delete unused native datas
+    # ==========================
     def _unlink_data(model, module, file_path):
         data_elements = etree.parse(file_open(file_path))
         model_elements = data_elements.xpath("//record[@model='{}']".format(model))
@@ -49,8 +52,33 @@ def post_init_hook(cr, registry):
     # Delete native crm stages
     _unlink_data("crm.stage", "crm", "crm/data/crm_stage_data.xml")
 
-    # -------------------------------------------------
     # Add all the users do 'group_use_lead' in order to display Leads for everybody
+    # =============================================================================
     users = env["res.users"].search([("id", "!=", env.ref("base.public_user").id)])
     group_use_lead = env.ref("crm.group_use_lead")
     group_use_lead.users = [(6, 0, users.ids)]
+
+    # Load demo records
+    # =================
+    if tools.config.get("demo_booking"):
+        tools.convert_file(
+            cr,
+            "crm_booking",
+            "demo/res.partner-demo.csv",
+            None,
+            mode="init",
+            noupdate=True,
+            kind="init",
+            report=None,
+        )
+
+        image_folder = "./odoo/external-src/crm-booking/crm_booking/static/img/"
+        image_files = {}
+        for file_name in listdir(image_folder):
+            if file_name.split("-")[1] == "demo":
+                image_files[file_name.split("-")[0]] = image_folder + file_name
+
+        for name, img_path in image_files.items():
+            with open(img_path, "rb") as file:
+                partner = env.ref("__import__.{}".format(name))
+                partner.image = base64.b64encode(file.read())
