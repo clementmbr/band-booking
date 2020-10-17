f0bd468be187e6737ce7ebfd6a5d52360ebf2235
clementmbr <clement.mombereau@akretion.com.br>
2020-10-17 10:57:53 -0300

    [REF] New module band_booking_usability

---
+++ band_booking_usability/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Band Booking Usability",
+    "summary": """Only XML views modifications for better UX""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Akretion",
+    "website": "http://akretion.com",
+    "depends": ["partner_structure"],
+    "data": ["views/contact_views.xml", "views/res_partner_views.xml"],
+    "demo": [],
+}
---
+++ band_booking_usability/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Improve User Experience for band booking.
+This module only move/replace elements fir views in Contact, CRM and Events applications.
